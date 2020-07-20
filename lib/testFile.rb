e4ec9696e025f334276ec4c1f3d4ff955ab5f30e
Alsouidan <alsouidan@gmail.com>
2020-07-20 16:18:06 +0200

    restoring relative requires

--- lib/testFile.rb
+++ lib/testFile.rb
@@ -1,4 +1,6 @@
 require_relative './gameball'
+require_relative './gameball/request'
+require_relative './gameball/getBot'
 Gameball::api_key="7c7636658209418c9a82306a421f76a5"
 Gameball::api_version="v1.0"
 res=Gameball::Request::GetBot::getBot
